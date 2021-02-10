c2a6c60d0c48128c6dc355d37ec9aa469fd41504
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-10 20:07:44 +0700

    Implements the view binding

--- app/src/main/java/com/erikriosetiawan/mywebview/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mywebview/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.erikriosetiawan.mywebview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.mywebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
