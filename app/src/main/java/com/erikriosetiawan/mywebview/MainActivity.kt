b915f87f8c25e4c8882347cd5cf7fe4fca6f6491
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-10 20:13:31 +0700

    Add a callback when page finished

--- app/src/main/java/com/erikriosetiawan/mywebview/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mywebview/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.erikriosetiawan.mywebview
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.mywebview.databinding.ActivityMainBinding
 
@@ -8,14 +12,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.webView.apply {
-            loadUrl("https://www.dicoding.com")
             settings.javaScriptEnabled = true
+
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            loadUrl("https://www.dicoding.com")
         }
     }
 }
