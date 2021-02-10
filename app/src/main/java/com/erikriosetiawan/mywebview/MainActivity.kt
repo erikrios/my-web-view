73a2f3f09b72a69f5663563c865406f083f98b08
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-02-10 20:14:56 +0700

    Add web chrome client

--- app/src/main/java/com/erikriosetiawan/mywebview/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mywebview/MainActivity.kt
@@ -2,9 +2,9 @@ package com.erikriosetiawan.mywebview
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.erikriosetiawan.mywebview.databinding.ActivityMainBinding
 
@@ -23,9 +23,11 @@ class MainActivity : AppCompatActivity() {
 
             webViewClient = object : WebViewClient() {
                 override fun onPageFinished(view: WebView?, url: String?) {
-                    Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG).show()
+                    view?.loadUrl("javascript:alert('Web Dicoding berhasil dimuat')")
                 }
             }
+
+            webChromeClient = WebChromeClient()
 
             loadUrl("https://www.dicoding.com")
         }
